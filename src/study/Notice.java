b6f04a2478cd00b8ff7bfc88f94d128d2cebd522
appleater7 <appleater7@gmail.com>
2019-02-14 13:55:52 +0900

    .

--- src/study/Notice.java
+++ src/study/Notice.java
@@ -4,8 +4,8 @@ package study;
  * 수업시간에 배운것에 대한 예제 작성과 풀이실습을 통한 반복학습 
  * 
  * ▶ 과제 출제 및 네이밍 규칙
- * MM + DD + Name
- * 예시) 0214RoomEscape
+ * MMM(영어) + DD(숫자) + Name
+ * 예시) Feb14RoomEscape
  * 
  * ▶ 과제 수행 규칙
  * study 패키지의 문제를 copy 후, 자신의 패키지에 복사후 문제 수행
