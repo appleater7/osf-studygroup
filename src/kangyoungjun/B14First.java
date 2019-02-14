2c29005036927c37cbc324752fb09088b8061138
appleater7 <appleater7@gmail.com>
2019-02-14 14:12:37 +0900

    .

---
+++ src/kangyoungjun/B14First.java
@@ -0,0 +1,12 @@
+package kangyoungjun;
+/*
+ * 클래스 영역에 int 형 변수 a(기본값0), String 형 변수 str(기본값"") 을 선언 및 초기화하고
+ * 메인 메서드에 Feb14First 클래스를 참조하는 변수 first 를 통하여,
+ * 콘솔에 "베스킨라빈스31"을 출력하시오!
+ * (단, 메인에는 변수선언을 하면 안된다.)
+ */
+public class B14First {
+
+	int a = 0;
+	String
+}
