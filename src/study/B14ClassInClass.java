2e188b693abe1a2bb26ac3138fd4c62e27bd8ec7
gitsunmin <tjsals116@gmail.com>
2019-02-14 16:29:46 +0900

    static class

---
+++ src/study/B14ClassInClass.java
@@ -0,0 +1,11 @@
+package study;
+
+public class B14ClassInClass {
+/*
+ * 
+ * 해당 class 안에 "B14ClassInClass2"라는 class를 만들고,
+ *  B14ClassInClass2 class 안에서 int a =1 ; 을 선언하여 println() 해 보시오.
+ *  HINT = ㅅㅌㅌ;
+ * 
+*/
+}
