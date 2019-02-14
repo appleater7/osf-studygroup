967c5b9c9436d79789a0d0928c7a2f402a305920
gitsunmin <tjsals116@gmail.com>
2019-02-14 13:59:42 +0900

    Study 제출

--- src/sunmin/Feb14First.java
+++ src/sunmin/Feb14First.java
@@ -5,6 +5,15 @@ package sunmin;
  * 콘솔에 "베스킨라빈스31"을 출력하시오!
  * (단, 메인에는 변수선언을 하면 안된다.)
  */
+	
 public class Feb14First {
-
+	int a =0;
+	String str = "";
+	
+	
+	public static void main(String[] args) {
+		Feb14First first = new Feb14First();
+		first.str="베스킨라빈스31";
+		System.out.println(first.str);
+	}
 }
