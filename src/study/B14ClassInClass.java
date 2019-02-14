37c79fee913c7a3ed37032d4e97f3f5d6d7cd0a3
gitsunmin <tjsals116@gmail.com>
2019-02-14 16:55:32 +0900

    ..

--- src/study/B14ClassInClass.java
+++ src/study/B14ClassInClass.java
@@ -4,7 +4,9 @@ public class B14ClassInClass {
 /*
  * 
  * 해당 class 안에 "B14ClassInClass2"라는 class를 만들고,
- *  B14ClassInClass2 class 안에서 int a =1 ; 을 선언하여 println() 해 보시오.
+ *  B14ClassInClass2 class 안에서 int a =1 ; 을 선언하여 
+ *  public static void main 에서 println() 해 보시오.
+ *  화이팅 입니당 ~!
  *  
  *  
  *  HINT = ㅅㅌㅌ;
