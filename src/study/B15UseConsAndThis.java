f22e209ba02809bb2defc4af9ac45817386c16aa
sunmin <tjsals116@gmail.com>
2019-02-17 23:37:17 +0900

    생성자와 this 연습

---
+++ src/study/B15UseConsAndThis.java
@@ -0,0 +1,61 @@
+package study;
+
+/*
+ * 아래의 코드에서 단 한 중만 코쳐서 
+ * "myNum is. 20"
+ * "mystr is. 나는 생성자"
+ * 를 출력하시오.
+ * 
+ * HINT는 맨 밑에 있으니 잘 안되면 활용하시오.
+ * 
+ * 
+ * 
+ * */
+
+
+
+public class B15UseConsAndThis {
+
+	int aNum;
+	String str;
+	
+	B15UseConsAndThis(){
+		
+	}
+	
+	
+	B15UseConsAndThis(int inNum, String inStr){
+		this.aNum = inNum;
+		this.str = inStr;
+		
+	}
+	
+	public static void main(String[] args) {
+		B15UseConsAndThis cat = new B15UseConsAndThis();
+		
+		System.out.println("myNum is. " + cat.aNum);
+		System.out.println("mystr is. " + cat.str);
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// HINT = parameter 
