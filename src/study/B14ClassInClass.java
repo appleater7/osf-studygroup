e235db5a8d4b0d4c743939541d88dad05bfd6a02
sunmin <tjsals116@gmail.com>
2019-02-17 23:55:05 +0900

    ㅇ

--- src/study/B14ClassInClass.java
+++ src/study/B14ClassInClass.java
@@ -2,14 +2,9 @@ package study;
 
 public class B14ClassInClass {
 /*
- * 
- * 해당 class 안에 "B14ClassInClass2"라는 class를 만들고,
- *  B14ClassInClass2 class 안에서 int a =1 ; 을 선언하여 
- *  public static void main 에서 println() 해 보시오.
- *  화이팅 입니당 ~!
  *  
  *  
- *  HINT = 그냥 답을 공개하고, 보면서 "이렇게도 할  수 있구나~"만 알아가는 목적으로 생각해요 ㅎㅎ
+ *  HINT = 그냥 답을 공개하고, 보면서 "이렇게도 할  수 있구나~"만 알아가는 목적으로...ㅎㅎ
  * 
 */
 	
