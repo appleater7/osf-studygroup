b0b558783c87952b1ea2ca61786a05bc2e71d2cb
BBongkook <plmqaz6153@gmail.com>
2019-02-21 21:37:38 +0900

    B14

---
+++ src/kimbongkook/B14First.java
@@ -0,0 +1,18 @@
+package kimbongkook;
+/*
+ * 클래스 영역에 int 형 변수 a(기본값0), String 형 변수 str(기본값"") 을 선언 및 초기화하고
+ * 메인 메서드에 B14First 클래스를 참조하는 변수 first 를 통하여,
+ * 콘솔에 "베스킨라빈스31"을 출력하시오!
+ * (단, 메인에는 변수선언을 하면 안된다.)
+ */
+
+public class B14First {
+	int a = 0;
+	String str = "";
+	public static void main(String[] args) {
+		B14First first = new B14First();
+		first.str="베스킨라빈스";
+		first.a=31;
+		System.out.println(first.str+first.a);	
+	}
+}
---
+++ src/kimbongkook/B14Two.java
@@ -0,0 +1,22 @@
+package kimbongkook;
+/*
+ * 생성자의 활용
+ * B14Two 의 멤버변수로 정수형 변수 abc 를 선언 및 10 으로 초기화 시키고, 
+ * 참조형의 문자열 변수 name 을 "" 의 값으로 선언 및 초기화 시킨다.
+ * 해당 클래스의 생성자를 이용하여 파라미터를 2개 문자,숫자의 순으로 전달 받으시오.
+ * 전달할 값은 문자는 "4", 숫자는 2로 하시오.
+ * 또 전달받은 정수형변수와 참조형 문자열 변수를 콘솔에 출력되게 하시오.
+ * (단, 메인메소드에서는 한번의 ';' 밖에 사용할 수 없다.
+ */
+public class B14Two {
+		int abc = 10;
+		String name = "";
+	B14Two(String str, int num){
+		abc=num;
+		name=str;
+		System.out.println(abc+"  "+name);
+	}
+	public static void main(String[] args) {
+		B14Two tw = new B14Two("4",2);
+	}
+}
