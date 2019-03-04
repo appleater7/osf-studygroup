46bad0c6b0676bc4326af2e7e315afe64cfbf41a
appleater7 <appleater7@gmail.com>
2019-03-04 09:43:07 +0900

    .

---
+++ src/quiz/B20CompareExam.java
@@ -0,0 +1,18 @@
+package quiz;
+//■ 2/19일 IntArray 클래스의 복습입니다.
+//해당 코딩을 암기하여 처음부터 구현하는것을 목표로 합니다.
+//
+//변수 intArray nums 를 클래스의 메인영역에 선언하고, 인덱스 영역은 10으로 초기화한다.
+//각 nums 배열의 각 인덱스에 무작위 값을 1 ~ 10 범위로 입력하는데
+//각 값은 중복되지 않도록 입력하여야 한다.
+//단, 값의 중복여부를 확인하는 로직은
+//boolean 의 data type 을 갖는 compare 메서드를 선언하여
+//메서드에서 기능 동작되도록 구현하고, 메인메서드에서 호출하여 사용하도록 한다.
+//By 영준
+
+public class B20CompareExam {
+	
+	public static void main(String[] args) {
+		
+	}
+}
