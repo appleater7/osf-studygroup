f87cdc76049bbacffb930bceba1e563adb1d4994
yoon22 <11yoon30@gmail.com>
2019-03-08 17:46:11 +0900

    11

--- src/yoon/B15GuGuDan.java
+++ src/yoon/B15GuGuDan.java
@@ -1,4 +1,7 @@
 package yoon;
+
+import java.util.Scanner;
+
 /*	
  * 1단부터 9단(1X9,1X8,1X7... 역순으로) 까지 출력은 메소드로 하시고 
  * 출력한 값을 배열에 직업놓고 Scanner으로 값을 입력 받으면
@@ -12,7 +15,31 @@ public class B15GuGuDan {
 	static int[][] a = new int[9][9];
 	
 	static public int[][] gugudan() {
+		for(int i=0;i<9;i++) {
+			for(int j=8; j>=0 ;j--) {
+				a[i][j] = (i+1)*(j+1);
+			}
+		}
+		return a;
+}
+	static void printGu() {
+		for(int i = 1 ; i<=9 ; i++) {
+			System.out.println();
+			for(int j = 9; j>0 ; j--) {
+				System.out.print(i + " x " + j + " = " + (i*j) + ",  " );
+			}
+		}
+		System.out.println();
+	}
+	
+	public static void main(String[] args) {
 		
+		printGu();
+		System.out.println("궁굼한거!");
+		Scanner scan = new Scanner(System.in);
+		int num = scan.nextInt();
+		
+		int[][] a = gugudan();
 		
 	}
 	
